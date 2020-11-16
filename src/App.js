8ac241c76fcd28ad7f8611a5f3a3250bd4c334d3
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-11-16 11:53:25 -0600

    Arreglado footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./routes/Router";
 
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <AppRouter />
+        <Footer/>
       </div>
     </Router>
   );
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import styled from "styled-components";
 
 const FooterContainer = styled.footer`
+  width: 90%;
   display: flex;
   justify-content: space-between;
+  position: absolute;
+  bottom: -80px;
 `;
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <p className="text-light">@abulnes16</p>
-      <p className="text-light">devchallenges.io</p>
+      <a href="https://github.com/abulnes16" className="link text-light">
+        @abulnes16
+      </a>
+      <a
+        href="https://devchallenges.io/challenges/N1fvBjQfhlkctmwj1tnw"
+        className="link text-light"
+      >
+        devchallenges.io
+      </a>
     </FooterContainer>
   );
 };
